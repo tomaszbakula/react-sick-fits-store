a7922c1c594428b7b8b4f5dc237762ac8380d7cd
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-16 22:35:12 +0100

    Basic frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <h1>Sell</h1>
+  </div>
+);
+
+export default Sell;
