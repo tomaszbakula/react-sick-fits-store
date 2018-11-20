1ced5d84ce8ff9e8fd029e8770cd1293be825d7f
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-20 15:01:35 +0100

    Create graphql interface

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check it they are logged in.
+
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   return ctx.db.query.items();
+  // }
+};
 
 module.exports = Query;
