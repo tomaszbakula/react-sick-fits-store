a146d7f1acbc822160b83c07ec19e595ba8e694c
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-23 15:00:12 +0100

    Update item api

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,6 +7,15 @@ const Mutations = {
     }, info);
 
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: { id: args.id }
+    }, info);
   }
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db')
   // async items(parent, args, ctx, info) {
   //   return ctx.db.query.items();
   // }
