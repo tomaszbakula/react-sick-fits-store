f1521d55a651ffd0d4142eeca4c248a86d98481c
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-26 13:58:24 +0100

    Pagination - add item connection.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   items: forwardTo('db'),
-  item: forwardTo('db')
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db')
   // async items(parent, args, ctx, info) {
   //   return ctx.db.query.items();
   // }
