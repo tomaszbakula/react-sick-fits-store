879af5353f27144447d884e43b1043e9005ce9c0
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-26 12:57:18 +0100

    Add the deleteItem mutation.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -16,6 +16,13 @@ const Mutations = {
       data: updates,
       where: { id: args.id }
     }, info);
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // TODO: Check if we own it.
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
