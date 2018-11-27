830b361b41499e3f870f7c5748160985390e7f92
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-27 17:24:25 +0100

    Add the "me" query.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,11 +2,17 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   items: forwardTo('db'),
+
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
-  // async items(parent, args, ctx, info) {
-  //   return ctx.db.query.items();
-  // }
+
+  itemsConnection: forwardTo('db'),
+
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) return null;
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId }
+    }, info);
+  }
 };
 
 module.exports = Query;
