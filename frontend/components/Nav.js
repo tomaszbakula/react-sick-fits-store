abb5c61baf0ccb53fe3dc04a35152e979c1d3c00
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-27 18:25:54 +0100

    Nav - display based on user login

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,25 @@
+import User from './User'
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-  <NavStyles>
-    <Link href="/items"><a>Shop</a></Link>
-    <Link href="/sell"><a>Sell</a></Link>
-    <Link href="/signup"><a>Signup</a></Link>
-    <Link href="/orders"><a>Orders</a></Link>
-    <Link href="/me"><a>Account</a></Link>
-  </NavStyles>
+    <User>
+      {({ data: { me }}) => (
+        <NavStyles>
+          <Link href="/items"><a>Shop</a></Link>
+          {me && (
+            <>
+            <Link href="/sell"><a>Sell</a></Link>
+            <Link href="/orders"><a>Orders</a></Link>
+            <Link href="/me"><a>Account</a></Link>
+            </>
+          )}
+          {!me && (
+            <Link href="/signup"><a>Sign In</a></Link>
+          )}
+        </NavStyles>
+      )}
+    </User>
 );
 
 export default Nav;
