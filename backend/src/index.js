46bf75e5850bf55b37ba5dadac5c295b38c63937
Tomasz Bakuła <tbakula92@gmail.com>
2018-11-27 17:24:09 +0100

    Parse the JWT token on the backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 const createServer = require('./createServer');
@@ -6,6 +7,16 @@ const db = require('./db');
 
 const server = createServer();
 server.express.use(cookieParser());
+
+// Decode the JWT so we can get the user Id on each request.
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: {
