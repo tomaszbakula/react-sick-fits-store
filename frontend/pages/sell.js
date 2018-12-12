021d679ee6b4cc885eac2fcf344cff8b7897a658
Tomasz Bakuła <tbakula92@gmail.com>
2018-12-12 11:02:59 +0100

    PleaseSignIn component.

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,12 @@
-import CreateItem from '../components/CreateItem'
+import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignIn';
 
-const Sell = props => (
+const SellPage = props => (
   <div>
-    <CreateItem />
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
   </div>
 );
 
-export default Sell;
+export default SellPage;
