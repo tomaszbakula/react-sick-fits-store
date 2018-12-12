ea71420da249b342b7fad39b79d769b4d637832d
Tomasz Bakuła <tbakula92@gmail.com>
2018-12-12 11:56:41 +0100

    Pull all users query.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -12,6 +13,18 @@ const Query = {
     return ctx.db.query.user({
       where: { id: ctx.request.userId }
     }, info);
+  },
+
+  async users(parent, args, ctx, info) {
+    // Check if the user is logged in.
+    if (!ctx.request.userId)
+      throw new Error('You must be logged in!');
+
+    // Check if the user has permissions ot query all users.
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    // Query all users
+    return ctx.db.query.users({}, info);
   }
 };
 
