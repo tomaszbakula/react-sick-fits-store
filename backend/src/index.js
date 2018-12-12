13bed669a066fe19f755c9c5cdd69ec9d1c3a5ed
Tomasz Bakuła <tbakula92@gmail.com>
2018-12-12 12:00:27 +0100

    Add a user object to the reqest.

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,19 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// Populate the user on each request.
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+
+  const user = await db.query.user(
+    { where: { id: req.userId }},
+    `{ id, name, email, permissions }`
+  );
+
+  req.user = user;
+  next();
+});
+
 server.start({
   cors: {
     credentials: true,
