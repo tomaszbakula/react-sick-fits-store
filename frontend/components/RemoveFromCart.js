0c7ed68770c5e3253190e2150c8456ceda7c147e
Tomasz Bakuła <tbakula92@gmail.com>
2018-12-14 15:11:09 +0100

    Remove from cart - optimistic response.

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -28,9 +28,29 @@ class RemoveFromCart extends Component {
     id: PropTypes.string.isRequired
   }
 
+  // This gets called as soon as we get a response back
+  // from server after the mutation has been performed.
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  }
+
   render() {
     return (
-      <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id: this.props.id }}>
+      <Mutation
+        mutation={REMOVE_FROM_CART_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id
+          }
+        }}
+      >
         {(removeFromCart, { loading, error }) => (
           <BigButton
             title="Delete Item"
