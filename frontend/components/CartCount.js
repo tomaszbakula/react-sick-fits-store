d20b29629c00bcf760c7315a326168bceae89ab7
Tomasz Bakuła <tbakula92@gmail.com>
2018-12-17 11:38:16 +0100

    The cart count animation.

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -3,6 +3,33 @@ import PropTypes from 'prop-types';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import styled from 'styled-components';
 
+const AnimationStyles = styled.span`
+  position: relative;
+
+  .count {
+    display: block;
+    position: relative;
+    transition: all .4s;
+    backface-visibility: hidden;
+  }
+
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+
+  .count-exit {
+    position: absolute;
+    top: 0;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
+
 const Dot = styled.div`
   background: ${props => props.theme.red};
   color: white;
@@ -17,7 +44,19 @@ const Dot = styled.div`
 `;
 
 const CartCount = ({ count }) => (
-  <Dot>{count}</Dot>
+  <AnimationStyles>
+    <TransitionGroup>
+      <CSSTransition
+        unmountOnExit
+        className="count"
+        classNames="count"
+        key={count}
+        timeout={{ enter: 400, exit: 400 }}
+      >
+        <Dot>{count}</Dot>
+      </CSSTransition>
+    </TransitionGroup>
+  </AnimationStyles>
 );
 
 Dot.propTypes = {
